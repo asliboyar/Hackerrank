436e09f06e6e2acaf71484f931f2b0778ac1075f
asli zeynep boyar <54912300+asliboyar@users.noreply.github.com>
2020-06-26 14:58:28 -0500

    Create 30day_code_3.py
    30 days of code
    day 3

---
+++ 30day_code_3.py
@@ -0,0 +1,16 @@
+import math
+import os
+import random
+import re
+import sys
+
+n = int(input())
+if n%2!=0:
+    print("Weird")
+else:
+    if 2<=n<=5:
+        print("Not Weird")
+    elif 6<=n<=20:
+        print("Weird")
+    else:
+        print("Not Weird")
