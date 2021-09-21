b5ccaf6aada73c0f2b88b33fc01a918daae23d69
asli zeynep boyar <54912300+asliboyar@users.noreply.github.com>
2021-09-21 16:44:15 -0500

    Update InsertionSort1.py

--- InsertionSort1.py
+++ InsertionSort1.py
@@ -16,15 +16,16 @@ import sys
 
 def insertionSort1(n, arr):
     # Write your code here
+    #this is the code of the inserion sort
     for i in range(1, len(arr)):
         key = arr[i]
         j = i- 1
         while j >= 0 and key < arr[j]:
             arr[j+1] = arr[j]
             j -= 1
-            print(*arr)
+            print(*arr) #this prints the every step while sorting
         arr[j+1] = key
-    print(*arr)
+    print(*arr) #final version of the array
             
 
 if __name__ == '__main__':
