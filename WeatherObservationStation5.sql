07257ea0e0519524ef77e818732a68fa539ca6cc
asli zeynep boyar <54912300+asliboyar@users.noreply.github.com>
2021-09-23 15:53:38 -0500

    Create WeatherObservationStation5.sql

---
+++ WeatherObservationStation5.sql
@@ -0,0 +1,5 @@
+/*
+Enter your query here.
+*/
+select city, length(city) from station order by length(city), city limit 1;
+select city, length(city) from station order by length(city) desc, city limit 1;
